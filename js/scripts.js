4ee8f11ff29ebd709a9fa763e4c401ace10ebeed
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-29 16:14:24 -0700

    Add functionality for currentID and function to increase it

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 // Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = {};
+  this.currentId = 0;
 }
 
 AddressBook.prototype.addContact = function(contact) {
   this.contacts[contact.firstName] = contact;
 }
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
 
 // Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
