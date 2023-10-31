434691e6411bf19555c4d8bf2f7513cd3edb419d
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-30 22:43:21 -0700

    Add listContacts function

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,20 @@ function AddressBook() {
   this.currentId = 0;
 }
 
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText =  null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  contactsDiv.append(ul);
+}
+
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
